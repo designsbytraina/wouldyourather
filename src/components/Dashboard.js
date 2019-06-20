6abfd93146682c80c74951e8478c4efb1fe8c8a7
Rachel Traina <racheltrainagrandon@gmail.com>
2019-06-19 17:39:58 -0700

    fix: Connect Dashboard and DashboardPoll to store and display correct data.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,26 +1,64 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
 import DashboardPoll from './DashboardPoll';
 import './Dashboard.css'
 
 class Dashboard extends React.Component {
   state = {
-    count: [ 1,2,3,4,5 ]
-  };
+    filterDashboard: 'unanswered'
+  }
+
+  toggleDashboard(e) {
+    const newView = e.target.value;
+    this.setState({filterDashboard: newView});
+  }
 
   render() {
+    let authedUserAnswered = [];
+    const authedUserQuestions = this.props.users[this.props.authedUser].questions;
+    authedUserQuestions.forEach( (q) => {
+        let answeredQuestion = this.props.questions[q];
+        authedUserAnswered.push(answeredQuestion);
+      }
+    )
+
+    let authedUserUnanswered = [];
+    Object.keys(this.props.questions).filter((q) => {
+      if (!authedUserQuestions.includes(q)) {
+        authedUserUnanswered.push(this.props.questions[q])
+      }
+    });
+
     return (
       <div className='Dashboard'>
-        <button className='unanswered dashboard-btn'>unanswered</button>
-        <button className='answered dashboard-btn'>answered</button>
-        <div className='dashboard-poll-list'>
-        {this.state.count.map( (c) => {
-          return <DashboardPoll key={c} />
-        } )}
-        </div>
+        <button onClick={this.toggleDashboard} className='unanswered dashboard-btn' value='unanswered'>unanswered</button>
+        <button onClick={this.toggleDashboard} className='answered dashboard-btn' value='answered'>answered</button>
+        {this.state.filterDashboard === 'unanswered'
+          ? <div className='dashboard-poll-list'>
+            {authedUserUnanswered.map( (q) => {
+              return <DashboardPoll key={q.id} id={q.id} />
+            } )}
+            </div>
+          : <div className='dashboard-poll-list'>
+            {authedUserAnswered.map( (q) => {
+              return <DashboardPoll key={q.id} id={q.id} />
+            } )}
+            </div>
+        }
       </div>
     )
   }
-
 }
 
-export default Dashboard;
+function mapStateToProps ({ questions, authedUser, users }) {
+  return {
+    authedUser,
+    users,
+    questions,
+    questionIds: Object.keys(questions)
+      .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+  }
+}
+
+export default connect(mapStateToProps)(Dashboard);
--- src/components/DashboardPoll.js
+++ src/components/DashboardPoll.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { formatQuestion } from '../utils/_DATA';
 import './DashboardPoll.css';
 
 class DashboardPoll extends React.Component {
   render() {
+    const { question } = this.props;
+    const author = question.author;
+    const optionOne = question.optionOne.text;
+    const optionTwo = question.optionTwo.text;
+
     return (
       <div className='DashboardPoll'>
         <div className='poll-card-img'>
           <img alt='placeholder' className='author-avatar' src='https://via.placeholder.com/290x290'/>
         </div>
         <div className='poll-card-body'>
-          <span className='author-name'>AUTHOR asks:</span>
+          <span className='author-name'>{author} asks:</span>
           <span className='wyr-body-text'>Would you rather ...</span>
-          <span className='wyr-body-options'><strong>optionA</strong></span> or <span className='wyr-body-options'><strong>optionB</strong></span>
+          <span className='wyr-body-options'><strong>{optionOne}</strong></span> or <span className='wyr-body-options'><strong>{optionTwo}</strong></span>
           <button className='dashboard-poll-btn btn'>view poll</button>
         </div>
       </div>
@@ -19,4 +26,21 @@ class DashboardPoll extends React.Component {
   }
 }
 
-export default DashboardPoll;
+function mapStateToProps ({authedUser, questions}, {id}) {
+  const question = questions[id];
+  const optionOneText = question.optionOne.text;
+  const optionTwoText = question.optionTwo.text;
+  const questionAuthor = question.author;
+
+  return {
+    authedUser,
+    question: question
+      ? question
+      : null,
+    votes: question
+      ? question.votes
+      : []
+  }
+}
+
+export default connect(mapStateToProps)(DashboardPoll);
